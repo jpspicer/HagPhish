76de1ee9346856543dc4fe9df90899d2b854be5b
Justin Spicer <js515@evansville.edu>
2019-11-23 01:03:57 -0500

    initial commit. .net core console app created.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HagPhish
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
